62ac18aa7c29480092308609f4013a69b1fdc78d
Vincent Behar <v.behar@free.fr>
2008-04-16 19:07:12 +0200

    test simpleconsole integration

---
+++ lib/nozber.rb
@@ -0,0 +1,12 @@
+# Add the directory containing this file to the start of the load path if it
+# isn't there already.
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+require 'nozbe'
+require 'nozber/cli'
+
+module Nozber
+  
+end
+
